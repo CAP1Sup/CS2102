3e77a64d5c09f9cee070b7d661abd16b4b52a576
Christian Piper <xmanpiper@gmail.com>
2023-03-14 03:36:00 -0400

    Worked on autograded HW0 problems
    Also cleaned up old class files, unneeded jar, and added gitignore

--- Homework_0/src/Examples.java
+++ Homework_0/src/Examples.java
@@ -1,11 +1,18 @@
-import org.junit.Assert; // make sure to use junit 4!!!
+import static org.junit.Assert.*; // make sure to use junit 4!!!
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * @author @CAP1Sup
+ * Note: These test cases were written with help from GitHub Co-Pilot
+ */
 
 public class Examples {
 
-    // It's better form to have 1 assert per test, but here are a few
+    // Total Kittens Method Tests
     @Test
     public void testTotalKittensPos() {
         int[] positiveTestData = { 1, 2, 3, 4 };
@@ -24,6 +31,7 @@ public class Examples {
         assertEquals("Failed total with empty array", 0, Rescue.totalKittens(emptyArray));
     }
 
+    // Average Puppy Age Method Tests
     @Test
     public void testAvgPuppyAgePos() {
         double[] positiveTestData = { 1, 2, 3, 4 };
@@ -40,5 +48,164 @@ public class Examples {
     public void testAvgPuppyAgeEmpty() {
         double[] emptyArray = {};
         assertEquals("Failed avg with empty array", 0d, Rescue.averagePuppyAge(emptyArray), 0.01d);
+    }
+
+    // Honorably Titled Method Tests
+    @Test
+    public void testHonorablyTitledFalseListedTitle() {
+        assertTrue("Detected false listed title", !Rescue.honorablyTitled("Title Fluffykins, MD"));
+    }
+
+    @Test
+    public void testHonorablyTitledFalsePeriodTitle() {
+        assertTrue("Detected false period title", !Rescue.honorablyTitled("Fluffykins, Esq."));
+    }
+
+    @Test
+    public void testHonorablyTitledFalseNoCredentials() {
+        assertTrue("Detected false no credentials", !Rescue.honorablyTitled("Judge Fluffykins"));
+    }
+
+    @Test
+    public void testHonorablyTitledTrue() {
+        assertTrue("Has both given title and credentials", Rescue.honorablyTitled("Count Fluffykins, Esq."));
+    }
+
+    // Chinchilla Feed Method Tests
+    @Test
+    public void testChinchillaFeedWithGoodNums() {
+        assertEquals("String formatting incorrect",
+                "Chinchilla: 4 lbs. of pellets | 13 oz. of hay | 22 units of dried fruit",
+                Rescue.chinchillaFeed(4, 13, 22));
+    }
+
+    @Test
+    public void testChinchillaFeedWithNegativePellets() {
+        assertEquals("Negative pellet count produces incorrect string printout",
+                "Chinchilla: unknown lbs. of pellets | 13 oz. of hay | 22 units of dried fruit",
+                Rescue.chinchillaFeed(-1, 13, 22));
+    }
+
+    @Test
+    public void testChinchillaFeedWithNegativeHay() {
+        assertEquals("Negative hay count produces incorrect string printout",
+                "Chinchilla: 5 lbs. of pellets | unknown oz. of hay | 10 units of dried fruit",
+                Rescue.chinchillaFeed(5, -1, 10));
+    }
+
+    @Test
+    public void testChinchillaFeedWithNegativeDriedFruit() {
+        assertEquals("Negative dried fruit count produces incorrect string printout",
+                "Chinchilla: 7 lbs. of pellets | 12 oz. of hay | unknown units of dried fruit",
+                Rescue.chinchillaFeed(7, 12, -1));
+    }
+
+    // Hearding Cats Method Tests
+    @Test
+    public void testHeardingCatsWithAllPositives() {
+        LinkedList<Coord> testData = new LinkedList<Coord>(
+                Arrays.asList(new Coord(1, 1), new Coord(10, 2), new Coord(6, 3)));
+        Coord testCoord = new Coord(2, 3);
+        assertEquals("Failed X with positive coords", 1, Rescue.heardingCats(testData, testCoord).x);
+        assertEquals("Failed Y with positive coords", 1, Rescue.heardingCats(testData, testCoord).y);
+    }
+
+    @Test
+    public void testHeardingCatsWithClosestX() {
+        LinkedList<Coord> testData = new LinkedList<Coord>(
+                Arrays.asList(new Coord(1, 1), new Coord(10, 2), new Coord(6, 20)));
+        Coord testCoord = new Coord(10, 15);
+        assertEquals("Failed X with closest X", 6, Rescue.heardingCats(testData, testCoord).x);
+        assertEquals("Failed Y with closest X", 20, Rescue.heardingCats(testData, testCoord).y);
+    }
+
+    @Test
+    public void testHeardingCatsWithClosestY() {
+        LinkedList<Coord> testData = new LinkedList<Coord>(
+                Arrays.asList(new Coord(1, 1), new Coord(10, 2), new Coord(6, 20)));
+        Coord testCoord = new Coord(7, 3);
+        assertEquals("Failed X with closest Y", 10, Rescue.heardingCats(testData, testCoord).x);
+        assertEquals("Failed Y with closest Y", 2, Rescue.heardingCats(testData, testCoord).y);
+    }
+
+    @Test
+    public void testHeardingCatsWithSameDistance() {
+        LinkedList<Coord> testData = new LinkedList<Coord>(
+                Arrays.asList(new Coord(2, 2), new Coord(10, 2), new Coord(6, 20)));
+        Coord testCoord = new Coord(6, 1);
+        assertEquals("Failed X with same distance", 10, Rescue.heardingCats(testData, testCoord).x);
+        assertEquals("Failed Y with same distance", 2, Rescue.heardingCats(testData, testCoord).y);
+    }
+
+    @Test
+    public void testHeardingCatsWithSomeNegatives() {
+        LinkedList<Coord> testData = new LinkedList<Coord>(
+                Arrays.asList(new Coord(-10, 1), new Coord(10, 2), new Coord(6, 3)));
+        Coord testCoord = new Coord(10, 1);
+        assertEquals("Failed X with some negative coords", 10, Rescue.heardingCats(testData, testCoord).x);
+        assertEquals("Failed Y with some negative coords", 2, Rescue.heardingCats(testData, testCoord).y);
+    }
+
+    @Test
+    public void testHeardingCatsWithEmptyList() {
+        LinkedList<Coord> testData = new LinkedList<Coord>();
+        assertEquals("Failed with empty list", null, Rescue.heardingCats(testData, new Coord(10, 1)));
+    }
+
+    // Goat Bloated Method Tests
+    @Test
+    public void testGoatBloatedWithNoBloats() {
+        ArrayList<Double> currentWeights = new ArrayList<Double>(Arrays.asList(15.0, 20.0, 25.0));
+        ArrayList<Double> targetWeights = new ArrayList<Double>(Arrays.asList(20.0, 25.0, 30.0));
+        double allowedError = 10.0;
+        assertEquals("Failed with no bloat case", 0, Rescue.goatBloat(currentWeights, targetWeights, allowedError));
+    }
+
+    @Test
+    public void testGoatBloatedWithErrorExclusivity() {
+        ArrayList<Double> currentWeights = new ArrayList<Double>(Arrays.asList(10.0));
+        ArrayList<Double> targetWeights = new ArrayList<Double>(Arrays.asList(20.0));
+        double allowedError = 10.0;
+        assertEquals("Failed with exclusivity case", 0, Rescue.goatBloat(currentWeights, targetWeights, allowedError));
+    }
+
+    @Test
+    public void testGoatBloatedWithUnderWeight() {
+        ArrayList<Double> currentWeights = new ArrayList<Double>(Arrays.asList(10.0));
+        ArrayList<Double> targetWeights = new ArrayList<Double>(Arrays.asList(20.0));
+        double allowedError = 5.0;
+        assertEquals("Failed with underweight case", 1, Rescue.goatBloat(currentWeights, targetWeights, allowedError));
+    }
+
+    @Test
+    public void testGoatBloatedWithOverWeight() {
+        ArrayList<Double> currentWeights = new ArrayList<Double>(Arrays.asList(20.0));
+        ArrayList<Double> targetWeights = new ArrayList<Double>(Arrays.asList(10.0));
+        double allowedError = 5.0;
+        assertEquals("Failed with overweight case", 1, Rescue.goatBloat(currentWeights, targetWeights, allowedError));
+    }
+
+    @Test
+    public void testGoatBloatedWithNegCurrentWeight() {
+        ArrayList<Double> currentWeights = new ArrayList<Double>(Arrays.asList(-1.0));
+        ArrayList<Double> targetWeights = new ArrayList<Double>(Arrays.asList(1.0));
+        double allowedError = 10.0;
+        assertEquals("Failed with negative current weight", 0, Rescue.goatBloat(currentWeights, targetWeights, allowedError));
+    }
+
+    @Test
+    public void testGoatBloatedWithNegTargetWeight() {
+        ArrayList<Double> currentWeights = new ArrayList<Double>(Arrays.asList(1.0));
+        ArrayList<Double> targetWeights = new ArrayList<Double>(Arrays.asList(-1.0));
+        double allowedError = 10.0;
+        assertEquals("Failed with negative current weight", 0, Rescue.goatBloat(currentWeights, targetWeights, allowedError));
+    }
+
+    @Test
+    public void testGoatBloatedWithUnevenLists() {
+        ArrayList<Double> currentWeights = new ArrayList<Double>(Arrays.asList(15.0, 10.0, 10.0, 30.0));
+        ArrayList<Double> targetWeights = new ArrayList<Double>(Arrays.asList(15.0, 20.0, 10.0));
+        double allowedError = 5.0;
+        assertEquals("Failed uneven list test", 1, Rescue.goatBloat(currentWeights, targetWeights, allowedError));
     }
 }
--- Homework_0/src/Rescue.java
+++ Homework_0/src/Rescue.java
@@ -4,9 +4,17 @@ import java.util.LinkedList;
 
 public class Rescue {
 
-    /////////////
-    // Autograded
+    // Listed titles for checking titling
+    static String listedTitles[] = { "Prince", "Princess", "Duke", "Dutchess", "Baron", "Baroness", "Count", "Countess",
+            "Judge" };
 
+    /**
+     * Returns the total number of kittens across all litters.
+     * Any litters with a value of less than 0 are not considered
+     *
+     * @param litters List of number of cats in each litter
+     * @return The sum of all kittens across the litters
+     */
     public static int totalKittens(int[] litters) {
         int sum = 0;
         for (int cats : litters) {
@@ -17,6 +25,13 @@ public class Rescue {
         return sum;
     }
 
+    /**
+     * Returns the average age from a list of puppy ages
+     * Ages less than 0 are assumed to be 0
+     *
+     * @param puppyAges List of ages of various puppies
+     * @return The average age of all of the puppies
+     */
     public static double averagePuppyAge(double[] puppyAges) {
         double sum = 0;
 
@@ -35,29 +50,179 @@ public class Rescue {
         }
     }
 
+    /**
+     * Checks if an animal's name is honorably titled (has both title and
+     * credential)
+     *
+     * @param name Name of animal
+     * @return If the animal is honorably titled
+     */
     public static boolean honorablyTitled(String name) {
-        // Delete and replace with your code:
-        throw new RuntimeException("HW Not Implemented");
+        if (hasListedTitle(name) || hasPeriodTitle(name)) {
+            if (hasCredentials(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 
+    /**
+     * Checks if the name has one of the provided titles
+     *
+     * @param name Name of animal
+     * @return If the animal's name has a listed title
+     */
+    private static boolean hasListedTitle(String name) {
+        for (String title : listedTitles) {
+            if (name.contains(title)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the name has a title which uses a period (such as Dr.)
+     * Titles with periods should have the period within the first 5 characters
+     *
+     * @param name Name of animal
+     * @return If the animal's name has a title using a period
+     */
+    private static boolean hasPeriodTitle(String name) {
+
+        // Find the first index of a period in the name (-1 if none)
+        int periodIndex = name.indexOf(".");
+
+        // If the name has a period title, then it must contain a period
+        // The period must also be within the first 5 characters
+        return (periodIndex != -1 && periodIndex < 5);
+    }
+
+    /**
+     * Checks if a name has credentials
+     * Credentials will always use a comma
+     * A comma will never exist unless the name has a credential
+     *
+     * @param name Name of animal
+     * @return If the animal's name has credentials
+     */
+    private static boolean hasCredentials(String name) {
+        // If a name contains a comma, it must have a credential
+        return (name.indexOf(",") != -1);
+    }
+
+    /**
+     * Concatenates pellet, hay, and dried fruit counts into single, labelled string
+     *
+     * @param pellets Pounds of pellets
+     * @param hay     Ounces of hay
+     * @param fruit   Units of dried fruit
+     * @return Concatenated string
+     */
     public static String chinchillaFeed(int pellets, int hay, int fruit) {
-        // Delete and replace with your code:
-        throw new RuntimeException("HW Not Implemented");
+        return String.format("Chinchilla: %s lbs. of pellets | %s oz. of hay | %s units of dried fruit",
+                convertCountToString(pellets),
+                convertCountToString(hay),
+                convertCountToString(fruit));
     }
 
+    /**
+     * Coverts a count of objects to a string
+     * Any counts less than 0 are converted to "unknown"
+     *
+     * @param count Number of objects
+     * @return String form of count
+     */
+    private static String convertCountToString(int count) {
+        if (count < 0) {
+            return "unknown";
+        }
+        return String.valueOf(count);
+    }
+
+    /**
+     * Returns the closest cat to the given location
+     * X and Y can be negative
+     * With an empty list, null is returned
+     * In the case of a tie, the coords that appear later will be returned
+     *
+     * @param catCoords List of cat coords
+     * @param location  Coordinates of given location
+     * @return The closest cat's coordinates
+     */
     public static Coord heardingCats(LinkedList<Coord> catCoords, Coord location) {
-        // Delete and replace with your code:
-        throw new RuntimeException("HW Not Implemented");
+        if (catCoords.size() == 0) {
+            return null;
+        }
+        double lowestDistance = Double.MAX_VALUE;
+        Coord lowestCatsCoord = new Coord(0, 0);
+        for (int index = 0; index < catCoords.size(); index++) {
+            double catsDistance = catDistance(catCoords.get(index), location);
+            if (catsDistance <= lowestDistance) {
+                lowestDistance = catsDistance;
+                lowestCatsCoord = catCoords.get(index);
+            }
+        }
+        return lowestCatsCoord;
     }
 
+    /**
+     * Returns the distance between a cat and a location
+     *
+     * @param cat      Cat's coordinates
+     * @param location Location's coordinates
+     * @return Distance between them
+     */
+    private static double catDistance(Coord cat, Coord location) {
+        return Math.sqrt(Math.pow(cat.x - location.x, 2) + Math.pow(cat.y - location.y, 2));
+    }
+
+    /**
+     * Returns the number of goats that are exclusively outside of the target weight
+     * error
+     * tolerance
+     * Any goats with current or target weights <= 0 are ignored
+     * If lists are of different lengths, the shorter list is used
+     * All units are in grams
+     *
+     * @param currentWeights List of current weights
+     * @param targetWeights  List of target weights
+     * @param errorTolerance Allowable weight tolerance
+     * @return Number of goats outside of error tolerance
+     */
     public static int goatBloat(ArrayList<Double> currentWeights, ArrayList<Double> targetWeights,
             double errorTolerance) {
-        // Delete and replace with your code:
-        throw new RuntimeException("HW Not Implemented");
+        int shortestListLen;
+        if (currentWeights.size() < targetWeights.size()) {
+            shortestListLen = currentWeights.size();
+        } else {
+            shortestListLen = targetWeights.size();
+        }
+
+        int bloatedGoatSum = 0;
+        for (int goatNum = 0; goatNum < shortestListLen; goatNum++) {
+            if (currentWeights.get(goatNum) > 0) {
+                if (targetWeights.get(goatNum) > 0) {
+                    if (goatBloated(currentWeights.get(goatNum), targetWeights.get(goatNum), errorTolerance)) {
+                        bloatedGoatSum++;
+                    }
+                }
+            }
+        }
+        return bloatedGoatSum;
     }
 
-    /////////////
-    // Design
-    // get Min and Max temps from a double[][]
-
+    /**
+     * Returns if a goat is bloated
+     * Goats are bloated when their current weight is over or under the target
+     * weight by more than the tolerance
+     *
+     * @param currentWeight  Goat's current weight
+     * @param targetWeight   Goat's target weight
+     * @param errorTolerance Tolerance for making weight
+     * @return If goat is bloated
+     */
+    private static boolean goatBloated(double currentWeight, double targetWeight, double errorTolerance) {
+        return (Math.abs(currentWeight - targetWeight) > errorTolerance);
+    }
 }
