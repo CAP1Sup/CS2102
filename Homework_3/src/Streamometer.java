0edeef657d8c3543e70223fa85a7cc0c2082ce35
Christian Piper <xmanpiper@gmail.com>
2023-04-06 17:26:35 -0400

    [HW3] Part 1 should be complete

--- Homework_3/src/Streamometer.java
+++ Homework_3/src/Streamometer.java
@@ -11,6 +11,10 @@ public class Streamometer {
         this.dailyRatings = dailyRatings;
     }
 
+    /**
+     * Finds the best rank across all platforms in the current month. Returns Integer.MAX_VALUE if no ranks exist for this month
+     * @return The best (lowest) rank this month
+     */
     public int bestRankThisMonth() {
         int lowestRank = Integer.MAX_VALUE;
         // Start from the end of the list, which will have the most recent data
@@ -21,16 +25,59 @@ public class Streamometer {
                 break;
             }
 
-            // Check if this days rank is lower
-            // TODO: Finish writing
+            // Check the if the day has a lower rank
+            for (Integer rank : dailyRatings.get(ratingIndex).ranks) {
+                if (rank < lowestRank) {
+                    lowestRank = rank;
+                }
+            }
         }
-        return -1;
+        return lowestRank;
     }
 
+    /**
+     * Returns the total number of subscribers gained across all platforms in the given month and year. 0 is returned if data doesn't exist for given month
+     * @param month Month number subscriptions occurred in, starting at 0
+     * @param year Year subscriptions occurred
+     * @return The total number of subscribers gained over the given month
+     */
     public int totalSubscribers(int month, int year) {
-        return -1;
+
+        int startingIndex = -1;
+        GregorianCalendar targetMonth = new GregorianCalendar(year, month, 0);
+
+        // Loop through until the starting index of the month is found
+        while (startingIndex < dailyRatings.size()) {
+            if (sameMonth(dailyRatings.get(startingIndex).date, targetMonth)) {
+                break;
+            }
+            startingIndex++;
+        }
+
+        // Starting index wasn't found, return 0
+        if (startingIndex == -1) {
+            return 0;
+        }
+
+        int totalSubscribers = 0;
+
+        // Loop through and add the subscribers
+        while (startingIndex < dailyRatings.size()) {
+            if (!sameMonth(dailyRatings.get(startingIndex).date, targetMonth)) {
+                break;
+            }
+            for (Integer subs : dailyRatings.get(startingIndex).subs) {
+                totalSubscribers += subs;
+            }
+        }
+
+        return totalSubscribers;
     }
 
+    /**
+     * Adds analytics from different platforms for a given day. Day is incremented with each call
+     * @param analytics List of analytics from different platforms
+     */
     public void addTodaysAnalytics(LinkedList<Analytics> analytics) {
         DailyRatings todaysRatings = new DailyRatings((GregorianCalendar) today.clone(), null, null);
         for (Analytics analytic : analytics) {
@@ -41,6 +88,12 @@ public class Streamometer {
         today.add(GregorianCalendar.DAY_OF_YEAR, 1);
     }
 
+    /**
+     * Checks if the calendars have the same month and year
+     * @param calendar First calendar to compare
+     * @param otherCalendar Second calender to compare
+     * @return If the first calendar and second calendar share the same month and year
+     */
     private boolean sameMonth(GregorianCalendar calendar, GregorianCalendar otherCalendar) {
         if (calendar.get(GregorianCalendar.YEAR) == otherCalendar.get(GregorianCalendar.YEAR)) {
             if (calendar.get(GregorianCalendar.MONTH) == otherCalendar.get(GregorianCalendar.MONTH)) {
